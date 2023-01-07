a1b0d9abf1c6beb267ef46d43d33e5cb019a0fbf
lappemic <61876623+lappemic@users.noreply.github.com>
2023-01-07 12:13:02 +0000

    correct hp_tuner.py

--- hw2_cnn_skeleton_code/hp_tuner.py
+++ hw2_cnn_skeleton_code/hp_tuner.py
@@ -1,15 +1,9 @@
 import os
 
-
-learning_rates = [0.00001, 0.0005, 0.01]
-hidden_sizes = [100, 200]
-dropout_probs = [0.3, 0.5]
-activation = ['relu', 'tanh']
-
 hp_search_arguments = ['-learning_rate 0.001 -optimizer adam', '-learning_rate 0.01 -optimizer adam', '-learning_rate 0.1 -optimizer adam']
 
 for arg in hp_search_arguments:
     print('****************************************************************************************************************')
     print('Training model with hp_search_arguments', arg)
     print('****************************************************************************************************************')
-    os.system('python hw1-q2.py mlp {}'.format(arg))
+    os.system('python hw2-q2.py mlp {}'.format(arg))
