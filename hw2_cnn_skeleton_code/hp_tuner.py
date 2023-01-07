d7d65533205d2e333ae5936bc25c9e74d2989c2a
lappemic <61876623+lappemic@users.noreply.github.com>
2023-01-07 12:15:43 +0000

    correct hp_tuner.py cli input

--- hw2_cnn_skeleton_code/hp_tuner.py
+++ hw2_cnn_skeleton_code/hp_tuner.py
@@ -6,4 +6,4 @@ for arg in hp_search_arguments:
     print('****************************************************************************************************************')
     print('Training model with hp_search_arguments', arg)
     print('****************************************************************************************************************')
-    os.system('python hw2-q2.py mlp {}'.format(arg))
+    os.system('python hw2-q2.py {}'.format(arg))
